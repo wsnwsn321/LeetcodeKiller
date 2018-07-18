3d4832f4fd73154118f3491668b5af3f9546ab4e
wsnwsn321 <wu.2482@osu.edu>
2018-07-18 17:20:51 +0800

    07/05/18

--- src/com/company/Solution500.java
+++ src/com/company/Solution500.java
@@ -9,23 +9,21 @@ public class Solution500 {
     }
     public static String[] findWords(String[] words) {
         ArrayList<String> result =new ArrayList<>();
-        boolean inRow = true;
         String[] rows = {"QWERTYUIOP","ASDFGHJKL","ZXCVBNM"};
         for(String v:words){
-            int i=0;
-           for(char c:v.toCharArray()) {
-               if (rows[i].indexOf(c) > 0) {
-                   continue;
-               } else {
-                   i++;
-               }
-               if (i == rows.length) {
-                   inRow = false;
-                   break;
-               }
-           }
-            if(inRow) result.add(v);
-           inRow = true;
+            int i=0,j=0;
+            int single =0;
+            if (rows[i].indexOf(c) > 0) {
+                single =i;
+                continue;
+            } else {
+                i++;
+                j=0;
+            }
+            for(char c: v.toCharArray()) {
+                if(i==rows.length) break;
+            }
+            if(single==i) result.add(v);
         }
         String[] rS = new String[result.size()];
         rS = result.toArray(rS);
