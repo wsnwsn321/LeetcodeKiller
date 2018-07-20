62a321db8d9fbf78013e3fe5d273de67f616b071
wsnwsn321 <wu.2482@osu.edu>
2018-07-20 17:58:20 +0800

    07/05/18

--- src/com/company/Solution500.java
+++ src/com/company/Solution500.java
@@ -1,33 +1,53 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution500 {
     public static void main(String[] args) {
-        String[] a = new String[]{"Hello","Alaska","Dad","Peace"};
+        String[] a = new String[]{"qaz","wsx","edc","rfv","tgb","yhn","ujm","ik","pklo","ppppppp"};
         System.out.println(findWords(a));
     }
     public static String[] findWords(String[] words) {
         ArrayList<String> result =new ArrayList<>();
+        ArrayList<String> newResult =new ArrayList<>(Arrays.asList(words));
         String[] rows = {"QWERTYUIOP","ASDFGHJKL","ZXCVBNM"};
+        ArrayList<String> alphabet = new ArrayList<String>(Arrays.asList(rows));
         for(String v:words){
-            int i=0,j=0;
-            int single =0;
-            if (rows[i].indexOf(c) > 0) {
-                single =i;
-                continue;
-            } else {
-                i++;
-                j=0;
+            String up = v.toUpperCase();
+            String check = "";
+            for(int i=0;i<up.length();i++){
+                if(rows[0].contains(up.substring(i,i+1)))
+                   check+=0;
+
+               else if(rows[1].contains(up.substring(i,i+1)))
+                    check+=1;
+
+               else if(rows[2].contains(up.substring(i,i+1)))
+                    check+=2;
             }
-            for(char c: v.toCharArray()) {
-                if(i==rows.length) break;
-            }
-            if(single==i) result.add(v);
+            result.add(check);
         }
-        String[] rS = new String[result.size()];
-        rS = result.toArray(rS);
-        return rS;
+        for(int i=0;i<result.size();i++){
+            String r = result.get(i);
+            for(char c:r.toCharArray()){
+                if(c!=r.toCharArray()[0]){
+                    newResult.set(i,"");
+                    break;
+                }
+            }
+        }
+        ArrayList<String> rS =new ArrayList<>();
+        for(String v:newResult){
+            if (!v.equals("") ){
+               rS.add(v);
+            }
+        }
+        String[] rt = new String[rS.size()];
+        rt = rS.toArray(rt);
+        return rt;
     }
 
 }
