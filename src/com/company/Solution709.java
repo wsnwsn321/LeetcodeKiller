4b3354d16026af2376d294792f93bafb07da744a
wsnwsn321 <wu.2482@osu.edu>
2018-07-26 17:52:25 +0800

    07/05/18

---
+++ src/com/company/Solution709.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Solution709 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public String toLowerCase(String str) {
+        String a = str.toLowerCase();
+        return a;
+    }
+}
