90c4c73837520207498003ba0a04946272cc78e0
wu.2482@osu.edu <7758258wsn>
2018-08-23 16:32:25 -0700

    mac

---
+++ src/com/company/Solution868.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Solution868 {
+    public static void main(String[] args) {
+
+    }
+    public static int binaryGap(int N) {
+        String bin = Integer.toBinaryString(N);
+
+    }
+}
--- src/com/company/Solution884.java
+++ src/com/company/Solution884.java
@@ -1,4 +1,57 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution884 {
+    public static void main(String[] args) {
+        String a ="fv kisxg hfmeq fw gi fpyc ojtr s hfmeq ojtr kisxg";
+        String b = "chpi hfmeq chpi dq hwtxa orql orql m s fw dq m fw";
+        String[] r = uncommonFromSentences(a,b);
+
+    }
+    public static String[] uncommonFromSentences(String A, String B) {
+        ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> a = new ArrayList<>();
+        a = seperator(A);
+        ArrayList<String> b = new ArrayList<>();
+        b = seperator(B);
+        for(String word:a){
+            if(!b.contains(word)&&Collections.frequency(a, word)==1){
+                result.add(word);
+            }
+        }
+        for(String word:b){
+            if(!a.contains(word)&&Collections.frequency(b, word)==1){
+                result.add(word);
+            }
+        }
+
+
+        String[] r = result.toArray(new String[result.size()]);
+        return r;
+
+
+    }
+
+    public static ArrayList<String> seperator(String a){
+        ArrayList<String> dup = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        int space = a.indexOf(" ");
+        int start = 0;
+        while(space!=-1){
+            String word = a.substring(start,space);
+            start = space+1;
+            space = a.indexOf(" ",start);
+            result.add(word);
+
+        }
+            result.add(a.substring(start));
+        return result;
+    }
+
+
 }
