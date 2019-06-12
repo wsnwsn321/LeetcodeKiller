4550b52a95e747a0770bd9a5ca0d5b56c738cf10
swu <tcwsn321@gmail.com>
2019-06-12 17:52:11 +0800

    6/12

--- src/com/company/Solution792_NumberofMatchingSubsequences.java
+++ src/com/company/Solution792_NumberofMatchingSubsequences.java
@@ -1,5 +1,7 @@
 package src.com.company;
 
+import java.util.Arrays;
+
 public class Solution792_NumberofMatchingSubsequences {
     public static void main(String[] args) {
         String s = "ricogwqznwxxcpueelcobbbku" +
@@ -25,21 +27,24 @@ public class Solution792_NumberofMatchingSubsequences {
     public static int numMatchingSubseq(String S, String[] words) {
         int count=0;
         boolean isWord =true;
+        int[] usedIndex = new int[26];
+        Arrays.fill(usedIndex,-1);
         for(String w:words){
             if(w.length()==1){
                 if(S.indexOf(w.charAt(0))>-1)
                     count++;
                 continue;
             }
-            int s =0,fp=0,sp=0;
+            int fp=0,sp=0;
             for(int i=0;i<w.length()-1;++i){
-                fp = S.indexOf(w.charAt(i),s);
+                fp = S.indexOf(w.charAt(i),usedIndex[w.charAt(i)-'a']+1);
+                usedIndex[w.charAt(i)-'a'] = fp;
                 sp = S.indexOf(w.charAt(i+1),fp);
+                usedIndex[w.charAt(i+1)-'a'] = sp;
                 if(sp<=fp){
                     isWord =false;
                     break;
                 }
-                s = fp;
             }
             if(isWord) count++;
             isWord = true;
