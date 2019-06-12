a7a87dcd5d6f457a8fa21d2b906a5e94bbab373f
swu <tcwsn321@gmail.com>
2019-06-12 15:41:56 +0800

    6/12

--- src/com/company/Solution792_NumberofMatchingSubsequences.java
+++ src/com/company/Solution792_NumberofMatchingSubsequences.java
@@ -2,27 +2,44 @@ package src.com.company;
 
 public class Solution792_NumberofMatchingSubsequences {
     public static void main(String[] args) {
-        String s = "abcde";
-        
+        String s = "ricogwqznwxxcpueelcobbbku" +
+                "vxxrvgyehsudccpsnuxpcqobtvwkuvsubiidjtccoqvuahijyefbpqhbejuisks" +
+                "utsowhufsygtwteiqyligsnbqglqblhpdzzeurtdohdcbjvzgjwylmmoiundjscnlhbr" +
+                "hookmioxqighkxfugpeekgtdofwzemelpyjsdeeppapjoliqlhbrbghqjezzaxuwyrbczo" +
+                "dtrhsvnaxhcjiyiphbglyolnswlvtlbmkrsurrcsgdzutwgjofowhryrubnxkahocqjzwwag" +
+                "qidjhwbunvlchojtbvnzdzqpvrazfcxtvhkruvuturdicnucvndigovkzrqiyastqpmfmuou" +
+                "ycodvsyjajekhvyjyrydhxkdhffyytldcdlxqbaszbuxsacqwqnhrewhagldzhryzdmmr" +
+                "wnxhaqfezeeabuacyswollycgiowuuudrgzmwnxaezuqlsfvchjfloczlwbefksxsbanrektvi" +
+                "bbwxnokzkhndmdhweyeycamjeplecewpnpbshhidnzwopdjuwbecarkgapyjfgmanuavzrxr" +
+                "icbgagblomyseyvoeurekqjyljosvbneofjzxtaizjypbcxnbfeibrfjwyjqrisuybfxpvqy" +
+                "wqjdlyznmojdhbeomyjqptltpugzceyzenflfnhrptuugyfsghluythksqhmxlmggtcbdddeo" +
+                "incygycdpehteiugqbptyqbvokpwovbnplshnzafunqglnpjvwddvdlmjjyzmwwxzjckmapti" +
+                "lrbfpjxiarmwalhbdjiwbaknvcqovwcqiekzfskpbhgxpyomekqvzpqyirelpadooxjhsyxjk" +
+                "fqavbaoqqvvknqryhotjritrkvdveyapjfsfzenfpuazdrfdofhudqbfnzxnvpluwicurrtsh" +
+                "yvevkriudayyysepzqfgqwhgobwyhxltligahroyshfndydvffd";
+        String[] words = new String[]{"ylmmo","bb","acd","ade"};
+        numMatchingSubseq(s,words);
+
+
     }
-    public int numMatchingSubseq(String S, String[] words) {
+    public static int numMatchingSubseq(String S, String[] words) {
         int count=0;
-        int[] firstIndex = new int[26];
-        java.util.Arrays.fill(firstIndex,-1);
-        int[] lastIndex = new int[26];
-        for(int i=0;i<S.length();++i){
-            if(firstIndex[S.charAt(i)-'a']==-1)
-                firstIndex[S.charAt(i) - 'a'] = i;
-            lastIndex[S.charAt(i) - 'a'] = i;
-        }
         boolean isWord =true;
         for(String w:words){
-            for(int i=0;i<w.length();++i){
-                if(firstIndex[S.charAt(i)-'a']>=lastIndex[S.charAt(i+1) - 'a']) {
-                    isWord = false;
+            if(w.length()==1){
+                if(S.indexOf(w.charAt(0))>-1)
+                    count++;
+                continue;
+            }
+            int s =0,fp=0,sp=0;
+            for(int i=0;i<w.length()-1;++i){
+                fp = S.indexOf(w.charAt(i),s);
+                sp = S.indexOf(w.charAt(i+1),fp);
+                if(sp<=fp){
+                    isWord =false;
                     break;
                 }
-
+                s = fp;
             }
             if(isWord) count++;
             isWord = true;
