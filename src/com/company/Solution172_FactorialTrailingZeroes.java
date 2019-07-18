c243bc95050f27b9e6caa1672c6021c5e25b04e3
swu <tcwsn321@gmail.com>
2019-07-18 17:34:30 +0800

    7/18

---
+++ src/com/company/Solution172_FactorialTrailingZeroes.java
@@ -0,0 +1,18 @@
+package src.com.company;
+
+public class Solution172_FactorialTrailingZeroes {
+    public static void main(String[] args) {
+
+    }
+    public int trailingZeroes(int n) {
+        public int trailingZeroes(int n) {
+            int r = 0;
+            while (n > 0) {
+                n /= 5;
+                r += n;
+            }
+            return r;
+        }
+    }
+
+}
