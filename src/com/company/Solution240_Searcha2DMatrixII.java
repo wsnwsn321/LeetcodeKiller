44f6393f84490cfa370424e998ac2d5780576e7e
swu <tcwsn321@gmail.com>
2019-07-25 17:30:29 +0800

    7/25

---
+++ src/com/company/Solution240_Searcha2DMatrixII.java
@@ -0,0 +1,13 @@
+package src.com.company;
+
+public class Solution240_Searcha2DMatrixII {
+    public static void main(String[] args) {
+
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int l =0,r=0;
+        
+    }
+}
