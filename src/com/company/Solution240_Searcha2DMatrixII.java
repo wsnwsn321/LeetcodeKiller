8cda31b1399ae612bb1f9c437d4c6fb71ea48d82
swu <tcwsn321@gmail.com>
2019-07-26 17:18:53 +0800

    7/26

--- src/com/company/Solution240_Searcha2DMatrixII.java
+++ src/com/company/Solution240_Searcha2DMatrixII.java
@@ -5,9 +5,18 @@ public class Solution240_Searcha2DMatrixII {
 
     }
     public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length < 1 || matrix[0].length <1) {
+            return false;
+        }
         int m = matrix.length;
         int n = matrix[0].length;
-        int l =0,r=0;
-        
+        int row = 0, col = n-1;
+        while(row<m&&col>=0){
+            if (matrix[row][col]==target) return true;
+            else if (matrix[row][col]>target) col--;
+            else row++;
+        }
+        return false;
+
     }
 }
---
+++ src/com/company/Solution260_SingleNumberIII.java
@@ -0,0 +1,27 @@
+package src.com.company;
+
+import java.util.*;
+
+public class Solution260_SingleNumberIII {
+    public static void main(String[] args) {
+        int[] n = new int[]{1,2,1,3,2,5};
+        singleNumber(n);
+    }
+    public static int[] singleNumber(int[] nums) {
+        Map<Integer,Integer> m = new HashMap<>();
+        Set<Integer> temp = new HashSet<>();
+        for (int x:nums){
+            temp.add(x);
+        }
+        for (int i=0;i<nums.length;++i){
+            m.put(nums[i],m.getOrDefault(nums[i],0)+1);
+            if (m.get(nums[i])==2) temp.remove(nums[i]);
+        }
+        int[] ret = new int[temp.size()];
+        List<Integer> temp2 = new ArrayList<>(temp);
+        for (int i=0;i<temp2.size();++i){
+            ret[i] =temp2.get(i);
+        }
+        return ret;
+    }
+}
