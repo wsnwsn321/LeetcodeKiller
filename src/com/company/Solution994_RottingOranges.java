c9d2b50fb8daa6081b4a6e264131d4f13c26c891
swu <tcwsn321@gmail.com>
2019-08-05 10:26:21 +0800

    8/2

---
+++ src/com/company/Solution994_RottingOranges.java
@@ -0,0 +1,10 @@
+package src.com.company;
+
+public class Solution994_RottingOranges {
+    public static void main(String[] args) {
+
+    }
+    public int orangesRotting(int[][] grid) {
+        
+    }
+}
